603fda2b362b331cf907a233d580394a890c7e5c
turnerjake <jaketurner@mail.fresnostate.edu>
2020-10-01 17:04:11 -0700

    Code refactor using for loop

--- class_grade.py
+++ class_grade.py
@@ -14,29 +14,40 @@ append
 
 print message to user """
 
+#grades = []
+
+#grade = input("Enter the 1st grade: ")
+#grades.append(float(grade))
+
+#grade = input("Enter the 2nd grade: ")
+#grades.append(float(grade))
+
+#grade = input("Enter the 3rd grade: ")
+#grades.append(float(grade))
+
+#grade = input("Enter the 4th grade: ")
+#grades.append(float(grade))
+
+#grade = input("Enter the 5th grade: ")
+#grades.append(float(grade))
+
+#grades.sort()
+
+#grades = grades[2:]
+
+#grades = sum(grades)
+
+#result = grades / 3
+
+#print("Average grade: {0:.2f}%".format(result))
+
 grades = []
 
-grade = input("Enter the 1st grade: ")
-grades.append(float(grade))
-
-grade = input("Enter the 2nd grade: ")
-grades.append(float(grade))
-
-grade = input("Enter the 3rd grade: ")
-grades.append(float(grade))
-
-grade = input("Enter the 4th grade: ")
-grades.append(float(grade))
-
-grade = input("Enter the 5th grade: ")
-grades.append(float(grade))
+for i in range(5):
+    print(i)
+    grades.append(float(input("Enter the next grade")))
 
 grades.sort()
+grades = sum(grades[2:]) / 3
 
-grades = grades[2:]
-
-grades = sum(grades)
-
-result = grades / 3
-
-print("Average grade: {0:.2f}%".format(result))
+print("Average grade: {0:.2f}%".format(grades))
